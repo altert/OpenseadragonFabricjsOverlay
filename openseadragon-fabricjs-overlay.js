9baa5608183f2eb4685e5acd904e29838cb8a9dd
Alexey Tikhonov <admin@rosphoto.org>
2017-07-23 12:49:30 +0300

    update OpenSeadragon to 2.3.0, with modification disabling pointer events to cirumvent chrome 55+ problems (see #11)

--- openseadragon-fabricjs-overlay.js
+++ openseadragon-fabricjs-overlay.js
@@ -10,7 +10,7 @@
 
     // ----------
     OpenSeadragon.Viewer.prototype.fabricjsOverlay = function() {
-     
+
 
         this._fabricjsOverlayInfo = new Overlay(this);
         return this._fabricjsOverlayInfo;
@@ -18,7 +18,7 @@
     // static counter for multiple overlays differentiation
     var counter = (function () {
         var i = 1;
-    
+
         return function () {
             return i++;
         }
@@ -26,7 +26,7 @@
     // ----------
     var Overlay = function(viewer) {
         var self = this;
-        
+
         this._viewer = viewer;
 
         this._containerWidth = 0;
@@ -41,24 +41,28 @@
         this._viewer.canvas.appendChild(this._canvasdiv);
 
         this._canvas = document.createElement('canvas');
-        
+
         this._id='osd-overlaycanvas-'+counter();
         this._canvas.setAttribute('id', this._id);
         this._canvasdiv.appendChild(this._canvas);
         this.resize();
         this._fabricCanvas=new fabric.Canvas(this._canvas);
         // disable fabric selection because default click is tracked by OSD
-        this._fabricCanvas.selection=false; 
+        this._fabricCanvas.selection=false;
         // prevent OSD click elements on fabric objects
         this._fabricCanvas.on('mouse:down', function (options) {
             if (options.target) {
+              
+                options.e.preventDefaultAction = true;
                 options.e.preventDefault();
                 options.e.stopPropagation();
             }
         });
-        
-    
-        
+
+
+
+
+
         this._viewer.addHandler('update-viewport', function() {
             self.resize();
             self.resizecanvas();
@@ -99,30 +103,30 @@
                 this._canvasdiv.setAttribute('height', this._containerHeight);
                 this._canvas.setAttribute('height', this._containerHeight);
             }
-            
+
         },
        resizecanvas: function() {
- 
-           var origin = new OpenSeadragon.Point(0, 0);         
+
+           var origin = new OpenSeadragon.Point(0, 0);
            var viewportZoom = this._viewer.viewport.getZoom(true);
            var image1 = this._viewer.world.getItemAt(0);
-           var zoom = image1.viewportToImageZoom(viewportZoom);     
-                   
+           var zoom = image1.viewportToImageZoom(viewportZoom);
+
            this._fabricCanvas.setWidth(this._containerWidth);
-           this._fabricCanvas.setHeight(this._containerHeight);        
+           this._fabricCanvas.setHeight(this._containerHeight);
            this._fabricCanvas.setZoom(zoom);
-                
-           var image1WindowPoint = image1.imageToWindowCoordinates(origin);        
+
+           var image1WindowPoint = image1.imageToWindowCoordinates(origin);
            var x=Math.round(image1WindowPoint.x);
            var y=Math.round(image1WindowPoint.y);
            var canvasOffset=this._canvasdiv.getBoundingClientRect();
-           
+
            var pageScroll = OpenSeadragon.getPageScroll();
-           
+
            this._fabricCanvas.absolutePan(new fabric.Point(canvasOffset.left - x + pageScroll.x, canvasOffset.top - y + pageScroll.y));
-           
+
        }
-        
+
     };
 
 })();
