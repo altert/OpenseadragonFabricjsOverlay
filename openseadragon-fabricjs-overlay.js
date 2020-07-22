7f5bda2988d883e48b2abe0846cc893e9233d090
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2020-07-22 10:24:39 -0400

    :shirt: But first, format. :coffee:

--- openseadragon-fabricjs-overlay.js
+++ openseadragon-fabricjs-overlay.js
@@ -1,12 +1,11 @@
 // OpenSeadragon canvas Overlay plugin 0.0.1 based on svg overlay plugin
 
-(function() {
+(function () {
 
     if (!window.OpenSeadragon) {
         console.error('[openseadragon-canvas-overlay] requires OpenSeadragon');
         return;
     }
-
 
     /**
      * @param {Object} options
@@ -15,14 +14,14 @@
      * @param {Number} options.scale
      *      Fabric 'virtual' canvas size, for creating objects
      **/
-    OpenSeadragon.Viewer.prototype.fabricjsOverlay = function(options) {
-
+    OpenSeadragon.Viewer.prototype.fabricjsOverlay = function (options) {
 
         this._fabricjsOverlayInfo = new Overlay(this);
         this._fabricjsOverlayInfo._scale = options.scale;
 
         return this._fabricjsOverlayInfo;
     };
+
     // static counter for multiple overlays differentiation
     var counter = (function () {
         var i = 1;
@@ -31,8 +30,9 @@
             return i++;
         }
     })();
+
     // ----------
-    var Overlay = function(viewer) {
+    var Overlay = function (viewer) {
         var self = this;
 
         this._viewer = viewer;
@@ -40,7 +40,7 @@
         this._containerWidth = 0;
         this._containerHeight = 0;
 
-        this._canvasdiv = document.createElement( 'div');
+        this._canvasdiv = document.createElement('div');
         this._canvasdiv.style.position = 'absolute';
         this._canvasdiv.style.left = 0;
         this._canvasdiv.style.top = 0;
@@ -50,13 +50,13 @@
 
         this._canvas = document.createElement('canvas');
 
-        this._id='osd-overlaycanvas-'+counter();
+        this._id = 'osd-overlaycanvas-' + counter();
         this._canvas.setAttribute('id', this._id);
         this._canvasdiv.appendChild(this._canvas);
         this.resize();
-        this._fabricCanvas=new fabric.Canvas(this._canvas);
+        this._fabricCanvas = new fabric.Canvas(this._canvas);
         // disable fabric selection because default click is tracked by OSD
-        this._fabricCanvas.selection=false;
+        this._fabricCanvas.selection = false;
         // prevent OSD click elements on fabric objects
         this._fabricCanvas.on('mouse:down', function (options) {
             if (options.target) {
@@ -67,39 +67,34 @@
             }
         });
 
-
-
-
-
-        this._viewer.addHandler('update-viewport', function() {
+        this._viewer.addHandler('update-viewport', function () {
             self.resize();
             self.resizecanvas();
 
         });
 
-        this._viewer.addHandler('open', function() {
+        this._viewer.addHandler('open', function () {
             self.resize();
             self.resizecanvas();
         });
-
 
     };
 
     // ----------
     Overlay.prototype = {
         // ----------
-        canvas: function() {
+        canvas: function () {
             return this._canvas;
         },
-        fabricCanvas: function() {
+        fabricCanvas: function () {
             return this._fabricCanvas;
         },
         // ----------
-        clear: function() {
+        clear: function () {
             this._fabricCanvas.clearAll();
         },
         // ----------
-        resize: function() {
+        resize: function () {
             if (this._containerWidth !== this._viewer.container.clientWidth) {
                 this._containerWidth = this._viewer.container.clientWidth;
                 this._canvasdiv.setAttribute('width', this._containerWidth);
@@ -113,24 +108,24 @@
             }
 
         },
-       resizecanvas: function() {
+        resizecanvas: function () {
 
-           var origin = new OpenSeadragon.Point(0, 0);
-           var viewportZoom = this._viewer.viewport.getZoom(true);
-           this._fabricCanvas.setWidth(this._containerWidth);
-           this._fabricCanvas.setHeight(this._containerHeight);
-           var zoom = this._viewer.viewport._containerInnerSize.x * viewportZoom / this._scale;
-           this._fabricCanvas.setZoom(zoom);
-           var viewportWindowPoint = this._viewer.viewport.viewportToWindowCoordinates(origin);
-           var x=Math.round(viewportWindowPoint.x);
-           var y=Math.round(viewportWindowPoint.y);
-           var canvasOffset=this._canvasdiv.getBoundingClientRect();
+            var origin = new OpenSeadragon.Point(0, 0);
+            var viewportZoom = this._viewer.viewport.getZoom(true);
+            this._fabricCanvas.setWidth(this._containerWidth);
+            this._fabricCanvas.setHeight(this._containerHeight);
+            var zoom = this._viewer.viewport._containerInnerSize.x * viewportZoom / this._scale;
+            this._fabricCanvas.setZoom(zoom);
+            var viewportWindowPoint = this._viewer.viewport.viewportToWindowCoordinates(origin);
+            var x = Math.round(viewportWindowPoint.x);
+            var y = Math.round(viewportWindowPoint.y);
+            var canvasOffset = this._canvasdiv.getBoundingClientRect();
 
-           var pageScroll = OpenSeadragon.getPageScroll();
+            var pageScroll = OpenSeadragon.getPageScroll();
 
-           this._fabricCanvas.absolutePan(new fabric.Point(canvasOffset.left - x + pageScroll.x, canvasOffset.top - y + pageScroll.y));
+            this._fabricCanvas.absolutePan(new fabric.Point(canvasOffset.left - x + pageScroll.x, canvasOffset.top - y + pageScroll.y));
 
-       }
+        }
 
     };
 
